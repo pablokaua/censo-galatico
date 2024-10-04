c2d6156fe2d1751e9a0db30194b2b1c72a7072af
Pablo Kaua <pablokauaahdev@gmail.com>
2024-10-04 18:32:26 -0300

    add planet info and styles

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 let result = document.getElementById("result")
 
+function getPlanetData(planet) {
+    document.getElementById("name").textContent = planet.name 
+    document.getElementById("climate").textContent ="Clima: " + planet.climate 
+    document.getElementById("population").textContent = "População: " + planet.population
+    document.getElementById("terrain").textContent ="Terreno: " + planet.terrain 
+}
+
 async function getAllPlanets(){
-    let baseUrl = "https://swapi.dev/api/planets?format=json"
-    let res = await fetch(baseUrl)
+    let url = "https://swapi.dev/api/planets?format=json"
+    let res = await fetch(url)
     let {results} = await res.json()
 
     results.forEach(planet => {
         let li = document.createElement("li")
         li.innerHTML = `
-        <button>${planet.name}</button>
+        <button class="button-info">${planet.name}</button>
         `
+
+        let button = li.querySelector(".button-info")
+        button.addEventListener("click", () => getPlanetData(planet))
+
         result.appendChild(li)
     });
 }
 
-getAllPlanets()
+document.addEventListener("DOMContentLoaded", getAllPlanets)
+
