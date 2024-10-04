aaa800e7e46e381ca48b5b588ead394a4a816ab5
Pablo Kaua <pablokauaahdev@gmail.com>
2024-10-04 17:36:06 -0300

    add button to each planet

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+let result = document.getElementById("result")
+
 async function getAllPlanets(){
     let baseUrl = "https://swapi.dev/api/planets?format=json"
     let res = await fetch(baseUrl)
     let {results} = await res.json()
 
-    console.log(results)
+    results.forEach(planet => {
+        let li = document.createElement("li")
+        li.innerHTML = `
+        <button>${planet.name}</button>
+        `
+        result.appendChild(li)
+    });
 }
 
 getAllPlanets()
